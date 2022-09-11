eefb8e466c661b4901de4d7de1ae687bdd8ff7a5
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-11 16:23:11 -0700

    c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the printf function
+ *
+ * Return: 0
+ */
+int main (0)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of char: %ld byte(s)\n", size of (c));
+	printf("Size of int: %ld byte(s)\n", size of (i));
+	printf("Size of long int: %ld bytes(s)\n", size of (li));
+	printf("Size of long long int: %ld byte(s)\n", size of (lli));
+	printf("Size of float: %ld byte(s)\n", size of (f));
+	return (0)
+}
