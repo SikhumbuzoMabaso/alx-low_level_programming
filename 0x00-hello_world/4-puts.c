9fac454bd71719668268d3538918e9fd661068f2
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-11 14:54:06 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in put function
+ *
+ * return:0 after printing the function
+ */
+int main(void)
+{
+	puts("\"Programming is like building  multilingual puzzle");
+	return (0);
+}
