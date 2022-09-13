91db8ca06c61365136b4492c15301b7e1f5962bb
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-13 02:54:33 -0700

    my variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	else if (n==0)
 	{
-		printf("%i is zero\n", n)
+		printf("%i is zero\n", n);
 	}
 	else if (n<0)
 	{
-		printf("%i is negative\n", n)
+		printf("%i is negative\n", n);
 	}
 
 	return (0)
