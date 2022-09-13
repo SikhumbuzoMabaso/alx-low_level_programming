e9213cc0e00be6ffb691313d376712c5cfb739e6
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-13 13:20:16 -0700

    my variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c<= 'z'; c++)
+		ptchar(c);
+
+	putchar('\n');
+	return(0)
+}			
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0.Positive anything is better than negative nothing
 1.The last digit
+2.I sometimes suffer from insomia, and when i cant fall asleep,I play what I call alphabet game.
