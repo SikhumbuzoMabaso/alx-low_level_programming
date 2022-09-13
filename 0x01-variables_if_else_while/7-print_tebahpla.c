f1801852a73c988275758de601e19331e6861056
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-13 15:17:32 -0700

    my variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all aphabets in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
