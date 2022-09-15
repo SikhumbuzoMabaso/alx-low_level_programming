30c95ac4d8779dbafc5508d07f0c7d7a9d8ad7c9
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-14 18:22:44 -0700

    my loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two values
+ * @n: the first number to be computed
+ * @n: the second number to be computed
+ *
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: the number to be computed
+ *
+ * Return: Absolute value of number or zero
+ */
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		int abs_value;
+
+	i	abs_value = c * -1;
+
+		return (abs_value)
+	}
+
+	return (c);
+}
