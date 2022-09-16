24b3229f647dea8867143ccd7ef17afe1cf8529d
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-16 15:21:12 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * mian - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -12,7 +13,8 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)
+	/*
+	 * while (i < 10)
 	 *{
 	 *putchar(i);
 	 *}
