303f523bab0f2a5d36207b9ff333b1f8f05b2105
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-16 15:30:10 -0700

    debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * largest_number - return the lardest of 3 numbers
+ * @a: first number
+ * @b: second number
+ * @c: third number
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+	
+	if  (a >= b && b >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (0);
+}
