da45cef6fef8d577831bb25915f1dfe297c00f5c
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-19 21:07:28 -0700

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,16 +4,15 @@
  * _strlen - Returns the lenght of a string
  * @s: String to count
  *
- * Return: String Lenght
+ * Return: String Length
  */
 int _strlen(char *s)
 {
 	int len = 0;
 
-	for (*s != '\0'; len++)
-	{
+	for (*(s + len )!= '\0')
+	
 		len++;
-	}
-
+	
 	return (len);
 }
