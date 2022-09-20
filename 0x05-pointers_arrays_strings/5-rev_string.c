905ee78293e56ffc11c1f13e0bcdcc5f6de9b2b3
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-20 00:55:37 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - function prints a string in reverse
+ * @s: the string to be printed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (k = 0; k < c; k++)
+	{
+		a++;
+	}
+	for (i = 0; i < (c/2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
