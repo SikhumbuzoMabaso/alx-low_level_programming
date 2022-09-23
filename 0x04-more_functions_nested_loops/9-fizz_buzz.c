612c12cfef42ba8bc32fad40262a690dbfa51de5
Sikhumbuzo Mabaso <sikhumbuzo.mabaso@gmail.com>
2022-09-22 23:47:20 -0700

    loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,40 +1,32 @@
-#include "holberton.h"
 #include <stdio.h>
 
 /**
-  * main - Prints a Fizz Buzz program
-  *
-  * Return: Always 0 (Success)
-  */
+ *main - program that prints either number
+ *or fizz or buzz or fizzBuzz
+ *
+ *
+ *Return: returns 0
+ */
 int main(void)
 {
-	int x;
+	int num;
 
-	for (x = 1; x <= 100; x++)
-	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+	while (num++ < 100)
+
+		if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizzBuzz ");
+
+		else if ((num % 3) == 0)
+			printf("Fizz ");
+		else if ((num % 5) == 0)
 		{
-			printf("FizzBuzz");
-		}
-		else if (x % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (x % 5 == 0)
-		{
-			printf("Buzz");
+			if (num != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
 		}
 		else
-		{
-			printf("%d", x);
-		}
-                if (x != 100)
-		{
-			printf(" ");
-		}
-	}
-
+			printf("%d ", num);
 	printf("\n");
-
 	return (0);
 }
